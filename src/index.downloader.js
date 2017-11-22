bddc114c3be3309ca96f3ddcafcdd5cd5c8ee48b
Damon Qin <qin.aeon@gmail.com>
2017-11-22 19:55:12 +0800

    HTMLElement test demo

--- src/index.downloader.js
+++ src/index.downloader.js
@@ -1,7 +1,3 @@
-/**
- * Created by XieTing on 17-6-6.
- */
-
 module.exports = PearDownloader;
 
 var inherits = require('inherits');
@@ -12,34 +8,78 @@ inherits(PearDownloader, Worker);
 function PearDownloader(urlStr, token, opts) {
     var self = this;
     if (!(self instanceof PearDownloader)) return new PearDownloader(urlStr, token, opts);
-    // if (!(self instanceof PearPlayer)) return new PearPlayer(selector, opts);
     if (typeof token === 'object') return PearDownloader(urlStr, '', token);
     Worker.call(self, urlStr, token, opts);
-
-
 }
 
-var PRDownloaderProto = Object.create(HTMLElement.prototype);
-PRDownloaderProto.createdCallback = function() {
-
-    var downloader = new PearDownloader(this.dataset.src, {
-        useMonitor: true
+class  PearDownloaderTag extends HTMLElement {
+  constructor() {
+    super();
+    this.progress = 0;
+    this.status = 'ready';
+    this.speed = 0;
+    this.addEventListener('click', e => {
+      if (this.disabled) {
+        return;
+      }
+      this.downloader = this.createDownloader();
+      this.downloaderLifeCycle();
     });
-    downloader.on('progress', function(p){
-      console.warn(p, '-------');
+  }
+
+  connectedCallback() {
+    this.textContent = "卧槽！！！ - ";
+  }
+
+  createDownloader() {
+    if (!this.hasAttributes('data-src')) {
+      console.error('Must set data-src attribuite!');
+      return false;
+    }
+
+    let downloader = new PearDownloader(this.dataset.src, {
+      algorithm: 'firstaid',      //核心算法,默认firstaid
+      interval: 5000,             //滑动窗口的时间间隔,单位毫秒,默认10s
+      slideInterval: 20,          //当前播放时间与已缓冲时间小于这个数值时触发窗口滑动,单位秒,默认20s
+      useDataChannel: true,       //是否开启data channel,默认true
+      dataChannels: 10,            //创建data channel的最大数量,默认2
+      useTorrent: true,
+      useMonitor: true,             //是否开启monitor,会稍微影响性能,默认false
+      ordered: true
+    })
+    return downloader;
+  }
+
+  downloaderLifeCycle() {
+    this.downloader.on('begin', () => {
+      this.status = 'ready';
+
+      let ev = new CustomEvent("progress");
+      this.dispatchEvent(ev);
     });
-    PRDownloaderProto.instance = downloader;
 
-}
-// PRDownloaderProto.attachedCallback = function() {
-//     console.warn('XTreehouseProto attached:'+this.dataset.useMonitor);
-//     console.log(this.dataset.src)
-//
-//     // downloader.call(this);
-// }
-PRDownloaderProto.detachedCallback = function() {
+    this.downloader.on("progress", (prog) => {
+      let percent = (prog * 100).toFixed(1) + '%';
+      this.progress = percent;
+      this.status = 'downloading';
 
+      let ev = new CustomEvent("progress");
+      this.dispatchEvent(ev);
+    });
+
+    this.downloader.on('meanspeed', (speed) => {
+      this.speed = Math.round(speed) + ' KB/S';
+    });
+
+    this.downloader.on('done', () => {
+      this.status = 'done';
+      
+      let ev = new CustomEvent("progress");
+      this.dispatchEvent(ev);
+    });
+  }
 }
-PRDownloaderProto.attributeChangedCallback = function(attrName, oldValue, newValue) {}
-var PRDownloader = document.registerElement('pr-downloader', { prototype: PRDownloaderProto });
-// PRDownloader.call(PearDownloader);
+
+if (!window.customElements.get('pear-downloader')) {
+  window.customElements.define('pear-downloader', PearDownloaderTag);
+}
